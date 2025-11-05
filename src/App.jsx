ac8ecb9e84d7e0f94615c007de3f37a4396eeaa7
Arun24-8 <arunbenuku118@gmail.com>
2025-11-05 19:53:22 +0530

    Your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,3 @@
-/* Quick Git push cheatsheet for this project:
-   1) First-time (local -> new remote):
-      git init
-      echo node_modules/ > .gitignore
-      git add .
-      git commit -m "Initial commit"
-      # create remote on GitHub (web or `gh repo create <name> --public --source=. --remote=origin --push`)
-      git remote add origin git@github.com:USERNAME/REPO.git   # or HTTPS
-      git branch -M main
-      git push -u origin main
-
-   2) Usual workflow:
-      git add .
-      git commit -m "short message"
-      git push
-
-   3) If remote has new changes:
-      git pull --rebase origin main
-*/
 
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
--- src/shoppingfolder/componenets/Collection.jsx
+++ src/shoppingfolder/componenets/Collection.jsx
@@ -1,26 +1,44 @@
- import React from 'react'
+import React, { useState } from 'react'
 
  
  const Collection = (props) => {
   const {title,image1,image2,image3,image4,image5,image6,price1,price2,price3,price4,price5,price6}=props.gentsdata;
-   return (
-     <div className='collectionsection'>
-      <h2>{title}</h2>
-      <div className='menImages'>
-      <img src={image1} alt='menfashion' />
-      <img src={image2} alt='menfashion' />
-
-      <img src={image3} alt='menfashion' />
+  const [showPrices, setShowPrices] = useState(false);
   
-      <img src={image4} alt='menfashion' />
- 
-      <img src={image5} alt='menfashion' />
- 
-      <img src={image6} alt='menfashion' />
-
-     </div>
-     </div>
-   )
+  return (
+    <div className='collectionsection'>
+      <h2>{title}</h2>
+      <button onClick={() => setShowPrices(s => !s)} className='priceToggle'>
+        {showPrices ? 'Hide Prices' : 'Show Prices'}
+      </button>
+      <div className='menImages'>
+        <div className='item'>
+          <img src={image1} alt='menfashion' />
+          {showPrices && <div className='price'>₹{price1}</div>}
+        </div>
+        <div className='item'>
+          <img src={image2} alt='menfashion' />
+          {showPrices && <div className='price'>₹{price2}</div>}
+        </div>
+        <div className='item'>
+          <img src={image3} alt='menfashion' />
+          {showPrices && <div className='price'>₹{price3}</div>}
+        </div>
+        <div className='item'>
+          <img src={image4} alt='menfashion' />
+          {showPrices && <div className='price'>₹{price4}</div>}
+        </div>
+        <div className='item'>
+          <img src={image5} alt='menfashion' />
+          {showPrices && <div className='price'>₹{price5}</div>}
+        </div>
+        <div className='item'>
+          <img src={image6} alt='menfashion' />
+          {showPrices && <div className='price'>₹{price6}</div>}
+        </div>
+      </div>
+    </div>
+  )
  }
  
  export default Collection
